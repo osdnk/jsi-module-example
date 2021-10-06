2cb39591d4a964fc82f5dc250afb5a8ca3804e30
osdnk <micosa97@gmail.com>
2021-10-07 01:40:30 +0300

    Configure iOS bindings

---
+++ ios/SampleModule.mm
@@ -0,0 +1,47 @@
+//
+//  SampleModule.m
+//  newfeatures
+//
+//  Created by Michał Osadnik on 07/10/2021.
+//
+
+
+#import <React/RCTBridge+Private.h>
+#import <React/RCTUtils.h>
+#import <ReactCommon/RCTTurboModuleManager.h>
+#import <jsi/jsi.h>
+#import <React/RCTView.h>
+#import <memory>
+#import <React/RCTBridgeModule.h>
+
+@interface SampleModule : NSObject <RCTBridgeModule>
+
+@end
+
+using namespace facebook;
+
+@implementation SampleModule
+@synthesize bridge = _bridge;
+RCT_EXPORT_MODULE()
+
++ (BOOL)requiresMainQueueSetup {
+  return YES;
+}
+
+- (void)setBridge:(RCTBridge *)bridge
+{
+  _bridge = bridge;
+  
+  RCTCxxBridge *cxxBridge = (RCTCxxBridge *)self.bridge;
+  if (!cxxBridge.runtime) {
+    return;
+  }
+  
+  auto callInvoker = bridge.jsCallInvoker;
+  
+
+  // install here
+}
+
+@end
+
